01384f44bf723d08c58f544bae27ce62d0ff4614
dwnm1 <raraducker@gmail.com>
2025-11-12 16:55:28 +0900

    updating sessionbuilder in the level of yaml

--- spark/jobs/spark_batch_dataset.py
+++ spark/jobs/spark_batch_dataset.py
@@ -66,11 +66,6 @@ def main():
     spark = (
         SparkSession.builder
         .appName("SparkBatchDataset")
-        .config("spark.jars.packages",
-                "org.apache.hadoop:hadoop-aws:3.3.4,"
-                "com.amazonaws:aws-java-sdk-bundle:1.12.262"
-        )
-        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
         .config("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY_ID"))
         .config("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_ACCESS_KEY"))
         .config("spark.hadoop.fs.s3a.endpoint", "s3.ap-northeast-2.amazonaws.com") 
