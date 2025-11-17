322732f4c5f2db8438fe597908efa715adefea03
raraduck <raraducker@gmail.com>
2025-11-18 02:19:10 +0900

    update

--- airflow/dags/backfill-dag.py
+++ airflow/dags/backfill-dag.py
@@ -25,7 +25,7 @@ with DAG(
     params={
         "machines": Param(
             default="all",
-            description="all 또는 ['machine-1-1~8','machine-2-1~9', 'machine-3-1~11'] 형태로 입력"
+            description="all 또는 ['machine-1-1','machine-1-8','machine-2-1','machine-2-9', 'machine-3-1','machine-3-11'] 형태로 입력"
         )
     },
 ) as dag:
